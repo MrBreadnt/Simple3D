7191374653e8267c8ae21afd2fe71bd878d97677
BuildTools <unconfigured@null.spigotmc.org>
2026-02-23 03:43:02 +0700

    Addeed camera and .obj

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 #include <windows.h>
 #include <cmath>
 
-#define WIDTH 16 * 6
-#define HEIGHT 9 * 3
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+#define WIDTH 16 * 20
+#define HEIGHT 9 * 10
 
 using namespace std;
 
@@ -41,24 +46,46 @@ struct Edge {
     }
 };
 
+struct Camera {
+    Point3D position;
+    Point3D rotation;
+    float fov;
+    
+    Camera() {
+        position = {0, 0, -5};
+        rotation = {0, 0, 0};
+        fov = 90.0f;
+    }
+};
+
 void setPixel(int x, int y, char c);
 void clearBuffer();
 void render();
 void setCursorVisible(bool visible);
 Point2D screenCoords(Point2D point);
-Point2D projectCoords(Point3D point);
+Point2D projectCoords(Point3D point, Camera& camera);
 void printPoint(Point2D point);
 void printBorder();
-void rotateObject(Object3D& object, Point3D center, float angle, char axis);
-void moveObject(Object3D& object, Point3D vector);
 void movePoint(Point3D& point, Point3D vector);
-void rotatePoint(Point3D& point, Point3D center, float angle, char axis);
 void rotateX(Point3D& point, float angle);
 void rotateY(Point3D& point, float angle);
 void rotateZ(Point3D& point, float angle);
 Point3D computeCenter(std::vector<Point3D> vertices);
 void drawLine(Point2D start, Point2D end);
-void drawObject(Object3D object);
+void drawObject(Object3D& object, Camera& camera);
+Point3D worldToCamera(Point3D worldPoint, Camera& camera);
+void rotateCamera(Camera& camera, float angle, char axis);
+void moveCamera(Camera& camera, Point3D vector);
+void drawText(int x, int y, const char* text);
+void drawCameraInfo(Camera& camera);
+bool loadObjFile(const char* filename, Object3D& object);
+void centerObject(Object3D& object);
+void scaleObject(Object3D& object, float scale);
+void rotatePoint(Point3D& point, Point3D center, float angle, char axis);
+void moveObject(Object3D& object, Point3D vector);
+void rotateObject(Object3D& object, Point3D center, float angle, char axis);
+Object3D generateCube(Point3D position);
+Object3D generateCube(Point3D position, Point3D rotation, float scale);
 
 HANDLE hConsole;
 vector<CHAR_INFO> screenBuffer((WIDTH + 2) * (HEIGHT + 2));
@@ -73,68 +100,61 @@ int main() {
     setCursorVisible(false);
 
     vector<Object3D> objects;
+    Camera camera;
+    Object3D model;
+    if (loadObjFile("model.obj", model)) {
+        scaleObject(model, 5);
+        centerObject(model);
+        moveObject(model, {0, 2, 2});
+        //objects.push_back(model);
+    }
 
-    Object3D cubee;
-    cubee.vertices = {
-        {1, 1, 1},
-        {1, -1, 1},
-        {-1, -1, 1},
-        {-1, 1, 1},
-        {1, 1, -1},
-        {1, -1, -1},
-        {-1, -1, -1},
-        {-1, 1, -1},
-    };
-    cubee.faces = {
-        {{1, 2, 3, 4}},
-        {{5, 6, 7, 8}},
-        {{1, 2, 6, 5}},
-        {{3, 4, 8, 7}},
-        {{1, 4, 8, 5}},
-        {{2, 3, 7, 6}}
-    };
-    cubee.rotationPoint = computeCenter(cubee.vertices);
-    float a = 5;
-    moveObject(cubee, {0, 0, 3});
-    float rotationSpeed = 5;
-    float moveSpeed = 0.1;
-    objects.push_back(cubee);
-    Object3D& cube = objects[0];
+    objects.push_back(generateCube({0, 0, 3}));
+    objects.push_back(generateCube({1, 0, 3}));
+    objects.push_back(generateCube({-1, 0, 3}));
+    objects.push_back(generateCube({0, 1, 3}));
+    objects.push_back(generateCube({0, 2, 3}));
+    objects.push_back(generateCube({0, 3, 3}));
+    objects.push_back(generateCube({1, 3, 3}));
+    objects.push_back(generateCube({-1, 3, 3}));
+    
+    float moveSpeed = 0.2;
+    float rotateSpeed = 2;
+    
     while (true) {
         clearBuffer();
 
-        for(Object3D object : objects){
-            drawObject(object);
+        for(auto& object : objects){
+            drawObject(object, camera);
         }
 
-        if (_kbhit()) {
-            char key = _getch();
-            
-            switch (key) {
-                case 'x': rotateObject(cube, cube.rotationPoint, rotationSpeed, 'x'); break;
-                case 'X': rotateObject(cube, cube.rotationPoint, -rotationSpeed, 'x'); break;
-                case 'y': rotateObject(cube, cube.rotationPoint, rotationSpeed, 'y'); break;
-                case 'Y': rotateObject(cube, cube.rotationPoint, -rotationSpeed, 'y'); break;
-                case 'z': rotateObject(cube, cube.rotationPoint, rotationSpeed, 'z'); break;
-                case 'Z': rotateObject(cube, cube.rotationPoint, -rotationSpeed, 'z'); break;
-                
-                case 'w': moveObject(cube, {0, moveSpeed, 0}); break;
-                case 's': moveObject(cube, {0, -moveSpeed, 0}); break;
-                case 'a': moveObject(cube, {-moveSpeed, 0, 0}); break;
-                case 'd': moveObject(cube, {moveSpeed, 0, 0}); break;
-                case 'q': moveObject(cube, {0, 0, moveSpeed}); break;
-                case 'e': moveObject(cube, {0, 0, -moveSpeed}); break;
-                
-                case 27:
-                    setCursorVisible(true);
-                    return 0;
-            }
+        if (GetAsyncKeyState('I') & 0x8000) rotateCamera(camera, -rotateSpeed, 'x');
+        if (GetAsyncKeyState('K') & 0x8000) rotateCamera(camera, rotateSpeed, 'x');
+        
+        if (GetAsyncKeyState('Y') & 0x8000) rotateCamera(camera, -rotateSpeed, 'y');
+        if (GetAsyncKeyState('U') & 0x8000) rotateCamera(camera, rotateSpeed, 'y');
+        
+        if (GetAsyncKeyState('W') & 0x8000) moveCamera(camera, {0, 0, moveSpeed});
+        if (GetAsyncKeyState('S') & 0x8000) moveCamera(camera, {0, 0, -moveSpeed});
+        if (GetAsyncKeyState('A') & 0x8000) moveCamera(camera, {-moveSpeed, 0, 0});
+        if (GetAsyncKeyState('D') & 0x8000) moveCamera(camera, {moveSpeed, 0, 0});
+        if (GetAsyncKeyState('Q') & 0x8000) moveCamera(camera, {0, -moveSpeed, 0});
+        if (GetAsyncKeyState('E') & 0x8000) moveCamera(camera, {0, moveSpeed, 0});
+        
+        if (GetAsyncKeyState(VK_OEM_PLUS) & 0x8000 || GetAsyncKeyState(VK_ADD) & 0x8000) 
+            camera.fov = min(150.0f, camera.fov + 5);
+        
+        if (GetAsyncKeyState(VK_OEM_MINUS) & 0x8000 || GetAsyncKeyState(VK_SUBTRACT) & 0x8000) 
+            camera.fov = max(30.0f, camera.fov - 5);
+        
+        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+            setCursorVisible(true);
+            return 0;
         }
 
         printBorder();
-        
+        drawCameraInfo(camera);
         render();
-        
         Sleep(50);
     }
 
@@ -142,7 +162,255 @@ int main() {
     return 0;
 }
 
-void drawObject(Object3D object){
+Object3D generateCube(Point3D position){
+    Object3D cube;
+    cube.vertices = {
+        {0.5, 0.5, 0.5},
+        {0.5, -0.5, 0.5},
+        {-0.5, -0.5, 0.5},
+        {-0.5, 0.5, 0.5},
+        {0.5, 0.5, -0.5},
+        {0.5, -0.5, -0.5},
+        {-0.5, -0.5, -0.5},
+        {-0.5, 0.5, -0.5},
+    };
+    cube.faces = {
+        {{1, 2, 3, 4}},
+        {{5, 6, 7, 8}},
+        {{1, 2, 6, 5}},
+        {{3, 4, 8, 7}},
+        {{1, 4, 8, 5}},
+        {{2, 3, 7, 6}}
+    };
+    cube.rotationPoint = computeCenter(cube.vertices);
+    moveObject(cube, position);
+    return cube;
+}
+
+Object3D generateCube(Point3D position, Point3D rotation, float scale){
+    Object3D cube;
+    cube.vertices = {
+        {0.5, 0.5, 0.5},
+        {0.5, -0.5, 0.5},
+        {-0.5, -0.5, 0.5},
+        {-0.5, 0.5, 0.5},
+        {0.5, 0.5, -0.5},
+        {0.5, -0.5, -0.5},
+        {-0.5, -0.5, -0.5},
+        {-0.5, 0.5, -0.5},
+    };
+    cube.faces = {
+        {{1, 2, 3, 4}},
+        {{5, 6, 7, 8}},
+        {{1, 2, 6, 5}},
+        {{3, 4, 8, 7}},
+        {{1, 4, 8, 5}},
+        {{2, 3, 7, 6}}
+    };
+    cube.rotationPoint = computeCenter(cube.vertices);
+    rotateObject(cube, cube.rotationPoint, rotation.x, 'x');
+    rotateObject(cube, cube.rotationPoint, rotation.y, 'y');
+    rotateObject(cube, cube.rotationPoint, rotation.z, 'z');
+    scaleObject(cube, scale);
+    moveObject(cube, position);
+    return cube;
+}
+
+void scaleObject(Object3D& object, float scale) {
+    for (auto& vertex : object.vertices) {
+        vertex.x *= scale;
+        vertex.y *= scale;
+        vertex.z *= scale;
+    }
+    object.rotationPoint = computeCenter(object.vertices);
+}
+
+void centerObject(Object3D& object) {
+    Point3D center = computeCenter(object.vertices);
+    for (auto& vertex : object.vertices) {
+        vertex.x -= center.x;
+        vertex.y -= center.y;
+        vertex.z -= center.z;
+    }
+    object.rotationPoint = {0, 0, 0};
+}
+
+bool loadObjFile(const char* filename, Object3D& object) {
+    object.vertices.clear();
+    object.faces.clear();
+    
+    ifstream file(filename);
+    if (!file.is_open()) {
+        return false;
+    }
+    
+    string line;
+    int lineNum = 0;
+    
+    while (getline(file, line)) {
+        lineNum++;
+        
+        if (line.empty()) continue;
+        
+        istringstream iss(line);
+        string type;
+        iss >> type;
+        
+        if (type == "v") {
+            Point3D vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+            object.vertices.push_back(vertex);
+        }
+        
+        else if (type == "f") {
+            Face face;
+            string vertexData;
+            
+            while (iss >> vertexData) {
+                replace(vertexData.begin(), vertexData.end(), '/', ' ');
+                istringstream viss(vertexData);
+                
+                int vertexIndex;
+                viss >> vertexIndex;
+                
+                if (vertexIndex < 0) {
+                    vertexIndex = object.vertices.size() + vertexIndex + 1;
+                }
+                
+                face.vertexIndices.push_back(vertexIndex);
+                
+                int dummy;
+                viss >> dummy;
+                viss >> dummy;
+            }
+            
+            if (face.vertexIndices.size() >= 3) {
+                object.faces.push_back(face);
+            }
+        }
+    }
+    
+    file.close();
+    
+    if (!object.vertices.empty()) {
+        object.rotationPoint = computeCenter(object.vertices);
+    }
+    
+    return true;
+}
+
+
+void drawCameraInfo(Camera& camera) {
+    char buffer[100];
+    sprintf(buffer, "Pos: X=%.1f Y=%.1f Z=%.1f", 
+            camera.position.x, camera.position.y, camera.position.z);
+    drawText(0, 0, buffer);
+    
+    sprintf(buffer, "Rot: X=%.1f Y=%.1f Z=%.1f", 
+            camera.rotation.x, camera.rotation.y, camera.rotation.z);
+    drawText(0, 1, buffer);
+    
+    sprintf(buffer, "FOV: %.0f", camera.fov);
+    drawText(0, 2, buffer);
+}
+
+Point3D worldToCamera(Point3D worldPoint, Camera& camera) {
+    Point3D relative = {
+        worldPoint.x - camera.position.x,
+        worldPoint.y - camera.position.y,
+        worldPoint.z - camera.position.z
+    };
+    
+    float yaw = camera.rotation.y * 3.14159 / 180;
+    float cosY = cos(yaw);
+    float sinY = sin(yaw);
+    
+    Point3D afterYaw;
+    afterYaw.x = relative.x * cosY - relative.z * sinY;
+    afterYaw.y = relative.y;
+    afterYaw.z = relative.x * sinY + relative.z * cosY;
+    
+    float pitch = camera.rotation.x * 3.14159 / 180;
+    float cosP = cos(pitch);
+    float sinP = sin(pitch);
+    
+    Point3D afterPitch;
+    afterPitch.x = afterYaw.x;
+    afterPitch.y = afterYaw.y * cosP - afterYaw.z * sinP;
+    afterPitch.z = afterYaw.y * sinP + afterYaw.z * cosP;
+    
+    float roll = camera.rotation.z * 3.14159 / 180;
+    float cosR = cos(roll);
+    float sinR = sin(roll);
+    
+    Point3D result;
+    result.x = afterPitch.x * cosR - afterPitch.y * sinR;
+    result.y = afterPitch.x * sinR + afterPitch.y * cosR;
+    result.z = afterPitch.z;
+    
+    return result;
+}
+
+void rotateCamera(Camera& camera, float angle, char axis) {
+    switch (axis) {
+        case 'x':
+            camera.rotation.x += angle;
+            break;
+        case 'y':
+            camera.rotation.y += angle;
+            break;
+    }
+    
+    if (camera.rotation.x > 360) camera.rotation.x -= 360;
+    if (camera.rotation.x < 0) camera.rotation.x += 360;
+    if (camera.rotation.y > 360) camera.rotation.y -= 360;
+    if (camera.rotation.y < 0) camera.rotation.y += 360;
+    if (camera.rotation.z > 360) camera.rotation.z -= 360;
+    if (camera.rotation.z < 0) camera.rotation.z += 360;
+}
+
+void moveCamera(Camera& camera, Point3D vector) {
+    float yaw = camera.rotation.y * 3.14159 / 180;
+    float pitch = camera.rotation.x * 3.14159 / 180;
+    
+    float cosY = cos(yaw);
+    float sinY = sin(yaw);
+    float cosP = cos(pitch);
+    float sinP = sin(pitch);
+    
+    Point3D worldVector;
+    
+    if (vector.z != 0) {
+        worldVector.x = vector.z * sinY * cosP;
+        worldVector.y = 0;
+        worldVector.z = vector.z * cosY * cosP;
+    }
+    
+    if (vector.x != 0) {
+        worldVector.x += vector.x * cosY;
+        worldVector.y += 0;
+        worldVector.z += -vector.x * sinY;
+    }
+    
+    if (vector.y != 0) {
+        worldVector.x += 0;
+        worldVector.y += vector.y;
+        worldVector.z += 0;
+    }
+    
+    camera.position.x += worldVector.x;
+    camera.position.y += worldVector.y;
+    camera.position.z += worldVector.z;
+}
+void drawText(int x, int y, const char* text) {
+    int i = 0;
+    while (text[i] != '\0') {
+        setPixel(x + i, y, text[i]);
+        i++;
+    }
+}
+
+void drawObject(Object3D& object, Camera& camera){
     vector<pair<int, int>> uniqueEdges;
 
     for (const Face& face : object.faces) {
@@ -169,8 +437,8 @@ void drawObject(Object3D object){
         Point3D currentPoint = object.vertices[uniqueEdges[j].first];
         Point3D nextPoint = object.vertices[uniqueEdges[j].second];
         
-        Point2D currentProjected = screenCoords(projectCoords(currentPoint));
-        Point2D nextProjected = screenCoords(projectCoords(nextPoint));
+        Point2D currentProjected = screenCoords(projectCoords(currentPoint, camera));
+        Point2D nextProjected = screenCoords(projectCoords(nextPoint, camera));
         
         drawLine(currentProjected, nextProjected);
     }
@@ -189,7 +457,8 @@ void moveObject(Object3D& object, Point3D vector){
     movePoint(object.rotationPoint, vector);
 }
 
-void movePoint(Point3D& point, Point3D vector){
+
+void movePoint(Point3D& point, Point3D vector) {
     point.x += vector.x;
     point.y += vector.y;
     point.z += vector.z;
@@ -231,7 +500,6 @@ void rotateY(Point3D& point, float angle) {
     float newX = point.x * cosA + point.z * sinA;
     float newZ = -point.x * sinA + point.z * cosA;
     
-    
     point.x = newX;
     point.z = newZ;
 }
@@ -244,8 +512,8 @@ void rotateZ(Point3D& point, float angle) {
     float newX = point.x * cosA - point.y * sinA;
     float newY = point.x * sinA + point.y * cosA;
     
-    point.y = newY;
     point.x = newX;
+    point.y = newY;
 }
 
 Point3D computeCenter(vector<Point3D> vertices) {
@@ -322,15 +590,18 @@ Point2D screenCoords(Point2D point) {
     return point;
 }
 
-Point2D projectCoords(Point3D point){
-    if (point.z == 0) return {-2, -2};
-    point.x /= point.z;
-    point.y /= point.z;
-    return {point.x, point.y};
-}
-
-void printPoint(Point2D point) {
-    setPixel((int)point.x, (int)point.y, '*');
+Point2D projectCoords(Point3D point, Camera& camera) {
+    Point3D cameraPoint = worldToCamera(point, camera);
+    
+    float fovRad = camera.fov * 3.14159 / 180;
+    float scale = 1.0f / tan(fovRad / 2);
+    
+    if (cameraPoint.z <= 0) return {-2, -2};
+    
+    float projectedX = (cameraPoint.x * scale) / cameraPoint.z;
+    float projectedY = (cameraPoint.y * scale) / cameraPoint.z;
+    
+    return {projectedX, projectedY};
 }
 
 void printBorder() {
